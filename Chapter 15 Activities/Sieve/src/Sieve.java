5c926358dcf97d96825c49e03c0a0651214fa539
Dylan Kosloski <dtkosloski@stu.naperville203.org>
2023-09-22 12:45:08 -0500

    finished sieve

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,4 +1,7 @@
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
 
 /**
@@ -11,10 +14,20 @@ public class Sieve
         Scanner in = new Scanner(System.in);
         System.out.println("Compute primes up to which integer?");
         int n = in.nextInt();
+        
+        System.out.println();
+        Set<Integer> numbers = new HashSet<>();
 
-        // Your work goes here
-        . . .
+        int num = 2;
+        while (num <= n){
+            numbers.add(num);
+            num++;
+        }
 
+        for(Integer number: numbers){
+            if(!(number%2 == 0 || number%3 == 0) || number == 2)
+                System.out.println(number);
+        }
 
 
 
