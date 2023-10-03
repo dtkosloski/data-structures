c3afa08581d8f11c56fec1844ce1b980ca0b07cf
Dylan Kosloski <dtkosloski@stu.naperville203.org>
2023-10-03 13:22:21 -0500

    notes for queues and stacks

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,21 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        //Create a priority queue of to do items
+        //a WorkOrder has an message ID that is used to determine priority
+        //A priority queue can only store Comparable objects
+        Queue<WorkOrder> toDo = new PriorityQueue<>();
+
+        toDo.add(new WorkOrder(3,"Water plants"));
+        toDo.add(new WorkOrder(2,"Make dinner"));
+        toDo.add(new WorkOrder(2, "Walk dog"));
+        toDo.add(new WorkOrder(9,"Play videogames"));
+        toDo.add(new WorkOrder(1, "Study for exams"));
+
+        System.out.println(toDo);//prints/adds in priority order
+
+        while (toDo.size() > 0){
+            System.out.println(toDo.remove());
+        }
     }
 }
--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,28 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        //Create a print queue of strings (using a linked list)
+        Queue<String> jobs = new LinkedList<>();
+
+        //add some print jobs
+        jobs.add("Joe: Mama's delicious recipes");
+        jobs.add("Cathy: Top Secret Document");
+
+        System.out.println("Printing: "+jobs.remove());
+
+        jobs.add("Cathy: Top Secret-er Document");
+        jobs.add("Joe: Grocery List");
+        jobs.add("Cathy: Secret Agent Stuff");
+
+        System.out.println("Printing: "+jobs.remove());
+
+        jobs.add("Boss: Hit List For Cathy");
+
+        //Print the rest of the jobs in the queue
+        //System.out.println(jobs);
+        while (jobs.size()>0){
+            String job = jobs.remove();
+            System.out.println("Printing: "+jobs.remove());
+        }
     }
 }
